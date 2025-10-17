0c9703b304136f339de31d932cc8d73d7b55c0e3
middwinantony <middwin@gmail.com>
2025-10-16 20:58:03 -0400

    update and delete methods done but html still needs to be done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -20,6 +20,25 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat = Flat.destroy
+    redirect_to flats_path
+  end
+
   private
 
   def flat_params
